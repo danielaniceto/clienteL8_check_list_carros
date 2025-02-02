1a67e239dde6e8af3372d93a73008d38dbefafbe
danielaniceto <danielgustavots@hotmail.com>
2025-02-02 16:07:41 -0300

    Subindo page de login

--- api.py
+++ api.py
@@ -1,4 +1,11 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/pageinicial")
+def login_page(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "name": "Usuário"})
