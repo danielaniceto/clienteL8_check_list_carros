393e43c5068be6b3d1a39d61d873b52354963200
danielaniceto <danielgustavots@hotmail.com>
2025-02-03 20:29:32 -0300

    Subindo alterações nas apis e na login controller

--- api.py
+++ api.py
@@ -1,16 +1,23 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from schemas.loginschema import LoginSchema
+from schemas.checklistschema import CheckListSchema
 from controllers.login_controller import LoginController
+from controllers.api_controller import CheckListController
 
 app = FastAPI()
 
 login_controller = LoginController()
+check_list_controller = CheckListController()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/login/{usuario}/{senha}", response_model=dict)
+@app.post("/login/{usuario}/{senha}", response_model=dict)
 async def login_page(data: LoginSchema):
     return JSONResponse(content= await login_controller.is_valid_login(data), status_code=200)
+
+@app.post("/checklist", response_model=dict)
+async def check_list(data: CheckListSchema):
+    return JSONResponse(content= await check_list_controller.is_check_list_car(data), status_code=200)
--- controllers/login_controller.py
+++ controllers/login_controller.py
@@ -13,7 +13,7 @@ logging.basicConfig(
 
 class LoginController:
     def __init__(self):
-        logging.info('LoginController initialized')
+        logging.info("LoginController initialized")
 
         if service_db is None:
             logging.critical("A conexão com banco de dados falhou!!!")
@@ -24,7 +24,7 @@ class LoginController:
     async def is_valid_login(self, data: LoginSchema) -> RedirectResponse:
         logging.info(f"Dados recebidos para criação: url={data.usuario}, title={data.senha}")
 
-        user = self.service_db.find_one("usuário": data.uusuario, "senha": data.senha)
+        user = self.service_db.find_one({"usuário": data.uusuario, "senha": data.senha})
 
         if user:
             logging.info("Validação de usuário e senha OK!!!")
