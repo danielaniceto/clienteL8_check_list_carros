37c15ffab9540e4e1cd5e79eae45bcd6b740e0f7
danielaniceto <danielgustavots@hotmail.com>
2025-02-04 12:54:32 -0300

    Subindo alterações na api_controller

--- controllers/api_controller.py
+++ controllers/api_controller.py
@@ -2,6 +2,7 @@ import logging
 from uuid import uuid4, UUID
 from database.mongo import DataBaseConnection
 from schemas.checklistschema import CheckListSchema
+from models.db_model import RequestModel
 
 service_db = DataBaseConnection.is_connction_db()["campos_check_list_carro"]
 
@@ -23,13 +24,33 @@ class CheckListController:
             "hodometro": data.hodometro,
             "nivel_combustivel": data.nivel_combustivel,
             "validade_ipva": data.validade_ipva,
-            "conservacao_veiculo": data.conservacao_veiculo
-        }
+            "conservacao_veiculo": data.conservacao_veiculo,
+            "ar_condicionado": data.ar_condicionado,
+            "cartao_abastecineto": data.cartao_abastecineto,
+            "chave_ignicao": data.chave_ignicao,
+            "cinto_seguranca": data.cinto_seguranca,
+            "farol_lanternas": data.farol_lanternas,
+            "limpeza_interior": data.limpeza_interior,
+            "parabrisa": data.parabrisa,
+            "pneus": data.pneus,
+            "chave_roda": data.chave_roda,
+            "retrovisor": data.retrovisor,
+            "tag_sem_parar": data.tag_sem_parar,
+            "triangulo_sinalizacao": data.triangulo_sinalizacao,
+            "vidros": data.vidros,
+            "macaco": data.macaco,
+            "funcionamento_geral": data.funcionamento_geral,
+            "limpador_parabrisa": data.limpador_parabrisa,
+            "esguicho_agua": data.esguicho_agua,
+            "rack_escada": data.rack_escada,
+            "estado_geral": data.estado_geral,
+            "descricao_varias": data.descricao_avarias
+                }
 
         return self.is_insert_dict_db(check_list)
 
     def is_insert_dict_db(self, check_list: RequestModel) -> dict:
-        service_db_recorder.insert_one(data_requisition)
-        del data_requisition['_id']
+        service_db.insert_one(check_list)
         logging.info("Dicionário salvo no banco de dados!!!")
-        return data_requisition
+        return check_list
+    
