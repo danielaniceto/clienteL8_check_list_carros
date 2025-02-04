6c9f7abf3b1b5c72939545bf648fbfa8d6b0ecc0
danielaniceto <danielgustavots@hotmail.com>
2025-02-04 12:31:04 -0300

    Subindo db_models atualizado

--- models/db_model.py
+++ models/db_model.py
@@ -1,12 +1,30 @@
-"""from pydantic import BaseModel
+from pydantic import BaseModel
 from uuid import uuid4
 
 class RequestModel(BaseModel):
     request_id_check_list: str = uuid4().hex
-    request_route: str
-    request_method: str
-    request_header: str = {}
-    request_parameters: str = {}
-    request_body: str
-    request_api_response: str = {}
-    request_datatime: str"""
+    placa_carro: str
+    nome_completo: str
+    trecho: str
+    data_ultima_manutencao: str
+    conservacao_veiculo: str
+    ar_condicionado: str
+    cartao_abastecineto: str
+    chave_ignicao: str
+    cinto_seguranca: str
+    farol_lanternas: str
+    limpeza_interior: str
+    parabrisa: str
+    pneus: str
+    chave_roda: str
+    retrovisor: str
+    tag_sem_parar: str
+    angulo_sinalizacao: str
+    vidros: str
+    macaco: str
+    funcionamento_geral: str
+    limpador_parabrisa: str
+    esguicho_agua: str
+    rack_escada: str
+    estado_geral: str
+    varias: str
